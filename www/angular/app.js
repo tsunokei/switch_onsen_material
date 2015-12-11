879de488854ece7e07d92f549d8e9883238599ee
Tsunokei2015 <tsunokeikei@gmail.com>
2015-12-12 01:16:54 +0900

    login signup スタイル変更

--- www/angular/app.js
+++ www/angular/app.js
@@ -53,6 +53,9 @@
       mynavigator.pushPage('groupAdd/index.html');
       console.log("ajaxしたい")
     }
+    this.pushToNewIR = function(){
+      mynavigator.pushPage('newir/index.html');
+    }
 
     this.getInfradInfo = function(){
           console.log("eeyan")
@@ -64,12 +67,9 @@
                 },
                 success: function(msg){
                   console.log(msg)
-                  window.hoge=msg
-                  window.hoge2=msg["response"]["infrareds"]
-                  var obj = JSON.stringify(hoge2);
+                  hoge2=msg["response"]["infrareds"]
+                  obj = JSON.stringify(hoge2);
                   localStorage.setItem("l_obj",obj);
-
-                  console.log(hoge2)
 
                 },
                 error: function(){
@@ -77,8 +77,8 @@
                 }
             });
 
-            var obj = localStorage.getItem("l_obj");
-            var hoge2 = JSON.parse(obj);
+            obj2 = localStorage.getItem("l_obj");
+            var hoge2 = JSON.parse(obj2);
             console.log(hoge2)
 
             $scope.infrad = hoge2;
@@ -109,6 +109,55 @@
         // });
       })
     }
+    this.login = function(){
+        $.ajax({
+          url: site_url + "/api/v1/auth/login.json",
+          type: "POST",
+          data: {
+            "email_or_screen_name": identifier,
+            "password": password
+          },
+          success: function(msg){
+            localStorage.setItem("switch-auth_token",msg["response"]["auth_token"]);
+            location.href = "../index.html"
+          },
+          error: function(error){
+            if(error.status == 404){
+              localStorage.setItem("switch-site_url","")
+              location.href = "../api/index.html"
+            }else{
+              text = "<ul>";
+              messages = error.responseJSON.meta.errors.forEach(function(err){
+                text += "<li class='error'>" + err.message + "</li>";
+              });
+              text += "</ul>"
+              $("#error_messages").html(text);
+            }
+          }
+        });
+    }
+    this.logout = function(){
+      $.ajax({
+              url: site_url + "/api/v1/auth/logout.json",
+              type:"DELETE",
+              data: {
+                "auth_token": localStorage.getItem("switch-auth_token")
+              },
+              success: function(msg){
+                localStorage.setItem("switch-auth_token","")
+                location.href = "./login/index.html"
+              },
+              error: function(){
+                if(error.status == 404){
+                  localStorage.setItem("switch-site_url","")
+                  location.href = "../api/index.html"
+                }else{
+                  alert("error!")
+                }
+              }
+      })
+    }
+
 
     // $(function(){
     //   console.log("hage")
